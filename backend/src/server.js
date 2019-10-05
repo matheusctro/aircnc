9f33994af74ae9c5220c06921697a81629adb2fa
Matheus <m.s.castro@hotmail.com>
2019-10-05 13:44:30 -0400

    frontend integrated

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -13,7 +15,9 @@ mongoose.connect('mongodb+srv://omnistack:oitudobem@omnistack-igufy.mongodb.net/
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
